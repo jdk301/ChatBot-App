82559893f9e280e426f708433a448c580edb7a0c
Justin Kim <jkim@justins-mbp.myfiosgateway.com>
2021-03-05 16:40:10 -0500

    Create React app in client folder, install dependencies axios redux concurrently

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const watsonRoutes = require("./routes/api/watson");
 app.use("/api/watson", watsonRoutes);
 
 // Start server
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("Server is running on port ", port);
+  console.log("Server is listening on port ", port);
 });
